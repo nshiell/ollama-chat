4e8ca9242dba858d9501b07a315f80fac45149c2
Nicholas Shiell <nicholas@thecoderegistry.com>
2025-11-02 15:57:19 +0000

    Rewrite State() class with Storage() backend

--- ollama_chat/state.py
+++ ollama_chat/state.py
@@ -15,6 +15,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+from __future__ import annotations
+
 import json, re
 from appdirs import *
 from os import path, remove
@@ -24,135 +26,145 @@ from .bindings import Bindings
 import glob
 
 
-dirs = AppDirs('ollama-chat', 'nshiell')
-user_config_file_path = join(dirs.user_config_dir, 'config.json')
+def filter_dict(spec:dict, source) -> dict:
+    if not isinstance(source, dict):
+        return {k: None for k in spec.keys()}
 
-
-class Settings:
-    def __init__(self, **kwargs):
-        self.items = {
-            'model_name' : kwargs['model_name'],
-            'style'      : kwargs['style'],
-            'context'    : kwargs['context'],
-            'url'        : kwargs['url'],
-            'font'       : kwargs['font'],
-            'font_size'  : kwargs['font_size']
-        }
-
-        self.bind = Bindings(['changed'])
-
-
-    def __setitem__(self, key, item):
-        if key in self.items:
-            self.items[key] = item
+    new_dict = {}
+    for name, data_type in spec.items():
+        if name in source and isinstance(source[name], data_type):
+            new_dict[name] = source[name]
         else:
-            raise KeyError('Key: "%s" not in settings' % key)
+            new_dict[name] = None
 
-    def __getitem__(self, key):
-        return self.items[key]
+    return new_dict
 
 
-    def update(self, *args, **kwargs):
-        for value in args:
-            for key, item in value.items():
-                try:
-                    self.__setitem__(key, item)
-                except KeyError:
-                    pass
+def try_read_json_file(file_path:str):
+    try:
+        return json.loads(open(file_path, "r").read())
+    except Exception as e:
+        print(e)
 
-        for key, value in kwargs.items():
-            try:
-                self.__setitem__(key, value)
-            except KeyError:
-                pass
+    return None
 
-        self.bind.trigger('changed')
 
-    def __iter__(self):
-        for key in self.items:
-            yield key, self.items[key]
+class Storage:
+    def __init__(self, *, dirs:Optional[AppDirs]=None) -> None:
+        self.dir = dirs if dirs else AppDirs('ollama-chat', 'nshiell')
+        self.config_file_path = join(self.dir.user_config_dir, 'config.json')
+        self._config = None
+        #self._conversations = None
+
+
+    @property
+    def config(self):
+        if not self._config:
+            self._config = try_read_json_file(self.config_file_path)
+        return self._config
+
+
+    @config.setter
+    def config(self, config):
+        self._config = config
+        open(self.config_file_path, "w").write(
+            json.dumps(config, indent=4, sort_keys=True)
+        )
+
+
+    def conversations(self):
+        if not self._conversations:
+            self._conversations = self._load_conversations()
+
+        for conversation_dict in self._conversations:
+            yield conversation_dict
+
+
+    def save_all_conversations(self, conversations:list):
+        for con in conversations:
+            filename = con.name + '.conversation.json'
+            path = join(self.dir.user_config_dir, filename)
+
+            if con.mark_for_deletion:
+                if exists(path):
+                    remove(path)
+            else:
+                json_text = json.dumps(dict(con), indent=4, sort_keys=True)
+                open(path, "w").write(json_text)
+
+
+    def load_conversations(self):
+        glob_path = glob.glob(join(
+            self.dir.user_config_dir,
+            '*.conversation.json'
+        ))
+
+        for con_path in glob_path:
+            name_match = re.findall(r'^.*/(.*)\.conversation\.json$', con_path)
+
+            if name_match:
+                data = try_read_json_file(con_path)
+                if data:
+                    data['name'] = name_match[0]
+                    yield data
 
 
 class State:
-    def __init__(self, default_model=None, colour_scheme=None):
-        self.settings = Settings(
-            model_name='mistral-nemo:latest',
-            style='Blue',
-            context='You are being used for the programmer in building the application',
-            url='http://127.0.0.1:11434',
-            font='Ubuntu',
-            font_size=12
-        )
-        self.conversations = []
-        load_state(self)
+    """
+    Holds the runtime configuration and chat history.
+    It can be loaded from and saved to a JSON files
+    """
+    def __init__(self, *, storage:Optional[Storage]=None) -> None:
+        self.storage = storage if storage else Storage()
+        self.settings = self._get_settings()
+        self.conversations = self.load_conversations()
 
 
-    def update(self, values):
-        for att in self.attributes:
-            setattr(self, att, values[att] if att in values else self[att])
+    def _get_settings(self):
+        settings = self.storage.config
 
-        self.bind.trigger('changed')
+        if settings is None:
+            settings = {
+                'model_name': 'mistral-nemo:latest',
+                'style': 'Blue',
+                'context': 'You are being used in a IM style chat program',
+                'url': 'http://127.0.0.1:11434',
+                'font': 'Arial',
+                'font_size': 12
+            }
+
+        return filter_dict({
+            'context': str,
+            'font': str,
+            'font_size': int,
+            'model_name': str,
+            'style': str,
+            'url': str,
+        }, settings)
 
 
-    def __iter__(self):
-        for key in self.items:
-            yield key, getattr(self, key)
+    def load_conversations(self):
+        conversations = []
+        for conversation_dict in self.storage.load_conversations():
+            conversations.append(Conversation(**conversation_dict))
+
+        return conversations
 
 
     def save(self):
-        save_state(self)
+        self.storage.save_all_conversations(self.conversations)
 
 
-
-def make_config_dir_if_not_exists():
-    if not os.path.exists(dirs.user_config_dir):
-        os.makedirs(dirs.user_config_dir)
+    def __getitem__(self, key):
+        return self.settings[key]
 
 
-def set_config(path, data):
-    make_config_dir_if_not_exists()
-    json_data = json.dumps(dict(data), indent=4, sort_keys=True)
-    open(path, "w").write(json_data)
+    def __setitem__(self, key, item):
+        if key not in self.settings:
+            raise KeyError('Key: "%s" not in settings' % key)
+
+        self.settings[key] = item
 
 
-def load_state(state):
-    config = get_config()
-    if isinstance(config, dict):
-        state.settings.update(config)
-
-
-    glob_path = glob.glob(join(dirs.user_config_dir, '*.conversation.json'))
-    for con_path in glob_path:
-        try:
-            match = re.search(r'^.*/(.*)\.conversation\.json$', con_path)
-            name = match.group(1)
-            con_data = json.load(open(con_path, 'r'))
-            con_data['name'] = name
-            state.conversations.append(Conversation(**con_data))
-
-        except Exception as e:
-            print(e)
-            continue
-
-
-def delete_file(path):
-    if exists(path):
-        os.remove(path)
-
-
-def save_state(state):
-    set_config(user_config_file_path, state.settings)
-    for con in state.conversations:
-        path = join(dirs.user_config_dir, con.name + '.conversation.json')
-        if con.mark_for_deletion:
-            delete_file(path)
-            state.conversations.remove(con)
-        elif con.messages:
-            set_config(path, con)
-
-
-def get_config():
-    if path.exists(user_config_file_path):
-        return json.loads(open(user_config_file_path, "r").read())
-
-    return None
+    def __getitem__(self, key):
+        return self.settings[key]
